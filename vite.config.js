337f4c84b1170a36a9eb1fc3448620a913887b6e
edrg2 <hibluesky1125@gmail.com>
2025-07-15 21:09:13 +0800

    CD done

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,13 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const repositoryName = process.env.GITHUB_REPOSITORY
+  ? process.env.GITHUB_REPOSITORY.split("/")[1]
+  : "";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: repositoryName ? `/${repositoryName}/` : "/",
   test: {
     environment: "jsdom",
     setupFiles: "./setupTests.js",
